3021fdbfce521ceddce797c675e84b5372c4f5e2
FoolChandYadav <foolchand45643@gmail.com>
2025-10-16 11:22:11 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=12345
+let accountEmail="foolchand@gmail.com"
+var accountPass="1234"
+accountCity="Delhi"
+let accountStates;
+
+
+//accountId=2
+//accountEmail="pradeep@gmail.com"//node 01_basic/01_variable.js
+//accountPass="5645"
+//console.log(accountEmail)
+console.table([accountId,accountEmail,accountPass,accountCity,accountStates])
+
+//Perfer not yo use Var
+//because of block issue scope and funcational scope.S
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+
+//alert("hello")  we are using node js not broswer.
+
+console.log("foolchand") ;// code readbilty high;
+console.log(3+3)
+
+
+//	0 is value not null;
+//JavaScript has 8 Datatypes
+//Number,
+// Bigint,
+// String="",
+// Boolean=true/false,
+// Null= Standalone value
+//Undifined =
+//Symbol=
+
+//object
+
+console.log(typeof "foolchand")
